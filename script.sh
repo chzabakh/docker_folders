02296c55a1c12ef3dc925adbad2cc6464c09470f
chzabakh <56484562+chzabakh@users.noreply.github.com>
2022-02-28 14:50:01 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,5 +6,4 @@ rm /etc/apache2/sites-available/000-default.conf
 cp /docker_folders/pf /etc/apache2/sites-available/000-default.conf
 a2enmod ssl
 a2ensite default-ssl
-service apache2 stop
-service apache2 start
+service apache2 reload
