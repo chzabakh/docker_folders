a0a0e7c51e791b0dddfb93010e5491c2225b292e
Charaf eddine Zabakh <chzabakh@e2r7p10.1337.ma>
2022-02-28 13:06:25 +0100

    pls work

--- script.sh
+++ script.sh
@@ -1,4 +1,9 @@
-#cat /etc/apache2/sites-available/000-default.conf | tr "nigga" $HOST_IP > /tp && cat /tp > /etc/apache2/sites-available/000-default.conf
+cat /docker_folders/p1 > /docker_folders/pf
+printf "Redirect "/" "https://$HOST_IP:443/\\n"" > /docker_folders/p2
+cat /docker_folders/p2 >> /docker_folders/pf
+cat /docker_folders/p3 >> /docker_folders/pf
+rm /etc/apache2/sites-available/000-default.conf
+cp /docker_folders/pf /etc/apache2/sites-available/000-default.conf
 a2enmod ssl
 a2ensite default-ssl
 service apache2 stop
